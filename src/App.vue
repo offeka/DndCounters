4d8cd4821be065658826b35791e699f58f1bf30e
Kovler10 <tal06030@googlemail.com>
2020-12-11 22:26:19 +0200

    added button to add new counter

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
 <template>
   <div id="app">
-    <button class="btn rounded-circle edit-button btn-dark" @click="changeMode">edit</button>
+    <button class="btn rounded-circle edit-button btn-dark" @click="changeMode">
+      edit
+    </button>
+    <button class="btn rounded-circle edit-button btn-dark" @click="addCounter">
+      Add Counter
+    </button>
     <CountersView class="view"></CountersView>
   </div>
 </template>
 
 <script lang="ts">
-import Vue, {VueConstructor} from "vue";
-import store from "@/store"
+import Vue, { VueConstructor } from "vue";
+import store from "@/store";
 import CountersView from "@/components/CountersView.vue";
 
 export default (Vue as VueConstructor).extend({
-  name: 'App',
+  name: "App",
   store,
-  components: {CountersView},
+  components: { CountersView },
   methods: {
     changeMode(): void {
-      this.$store.commit("changeMode")
-    }
-  }
-})
+      this.$store.commit("changeMode");
+    },
+    addCounter(): void {
+      this.$store.commit("addCounter", {
+        name: "new Counter",
+        maxCount: 10,
+        currentCount: 0,
+        resetOn: "ShortRest",
+      });
+    },
+  },
+});
 </script>
 
 <style>
