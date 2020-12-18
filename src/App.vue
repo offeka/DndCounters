d65329c06294e3193388de256a833e8aac8a4788
Kovler10 <tal06030@googlemail.com>
2020-12-18 23:46:18 +0200

    modal moved to component and improved FAB styling

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,7 @@
     >
       +
     </button>
-    <modal modal-name="add-button" :title="true">
-      <template v-slot:modal-header>
-        Create new Counter
-      </template>
-      <template v-slot:modal-body>
-        <div class="input-group">
-          <input
-            type="text"
-            class="form-control"
-            placeholder="counter name"
-            aria-label="counter name"
-          />
-        </div>
-      </template>
-      <template v-slot:modal-footer>
-        <div class="form-footer">
-          <button
-            data-toggle="modal"
-            data-target="#add-button"
-            class="btn btn-success"
-          >
-            Ok
-          </button>
-          <button
-            data-toggle="modal"
-            data-target="#add-button"
-            class="btn btn-dark"
-          >
-            Close
-          </button>
-        </div>
-      </template>
-    </modal>
+    <counter-input></counter-input>
   </div>
 </template>
 
@@ -50,12 +18,12 @@ import Vue, { VueConstructor } from "vue";
 import store from "@/store";
 import CountersView from "@/components/CountersView.vue";
 import NavBar from "@/components/NavBar.vue";
-import Modal from "@/components/Modal.vue";
+import CounterInput from "@/components/CounterInput.vue";
 
 export default (Vue as VueConstructor).extend({
   name: "App",
   store,
-  components: { CountersView, NavBar, Modal },
+  components: { CountersView, NavBar, CounterInput },
 });
 </script>
 
@@ -65,18 +33,25 @@ export default (Vue as VueConstructor).extend({
 
 .floating-button {
   border-radius: 96px;
-  background: linear-gradient(145deg, #2ac000, #23a100);
-  box-shadow: 11px 11px 11px #23a100, -11px -11px 11px #2bc500;
+  background: #2ac000;
   position: fixed;
   width: 60px;
   height: 60px;
   bottom: 7vh;
+  font-size: 46px;
+  user-select: none;
+  line-height: 1;
+  padding-bottom: 15px;
   color: white;
   left: 75vw;
   text-align: center;
   outline: none;
-  box-shadow: 2px 2px 3px #999;
 }
+
+.floating-button:focus {
+  outline: none;
+}
+
 .form-footer {
   display: flex;
   justify-content: center;
