1b37003de9982af1e9e42602dcd791dbff43ce7a
Kovler10 <tal06030@googlemail.com>
2020-12-29 21:41:00 +0200

    hopefully fixed css on mobile

--- src/components/CountersView.vue
+++ src/components/CountersView.vue
@@ -1,33 +1,46 @@
 <template>
-  <div>
+  <div id="container">
     <div v-if="mode === 'view'">
-      <counter class="counter" v-for="index in countersCount" v-bind:index="index-1" :key="index"></counter>
+      <counter
+        class="counter"
+        v-for="index in countersCount"
+        v-bind:index="index - 1"
+        :key="index"
+      ></counter>
     </div>
     <div v-else>
-      <editable-counter v-for="index in countersCount" :index="index-1" :key="index"></editable-counter>
+      <editable-counter
+        v-for="index in countersCount"
+        :index="index - 1"
+        :key="index"
+      ></editable-counter>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue, {VueConstructor} from "vue";
-import {mapState} from "vuex";
+import Vue, { VueConstructor } from "vue";
+import { mapState } from "vuex";
 import EditableCounter from "@/components/EditableCounter.vue";
 import Counter from "@/components/Counter.vue";
 
 export default (Vue as VueConstructor).extend({
   name: "CountersView",
-  components: {Counter, EditableCounter},
+  components: { Counter, EditableCounter },
   computed: {
     countersCount(): number {
       return this.$store.state.counters.length;
     },
-    ...mapState(["mode"])
-  }
-})
+    ...mapState(["mode"]),
+  },
+});
 </script>
 
 <style scoped>
-.counter {
+#container {
+  position: fixed;
+  right: 50%;
+  left: 50%;
+  top: 12%;
 }
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="container">
     <div id="title-container">
-      <img id="logo" src="@/assets/logo.png"/>
+      <img id="logo" src="@/assets/logo.png" />
       <div id="title">{{ appTitle }}</div>
     </div>
     <div id="buttons-container">
       <button @click="changeMode" class="navbar-button">
         {{ this.$store.state.mode === "edit" ? "Save" : "Edit" }}
       </button>
-      <button class="navbar-button" data-toggle="modal" data-target="#rest-modal">
+      <button
+        class="navbar-button"
+        data-toggle="modal"
+        data-target="#rest-modal"
+      >
         Rest
       </button>
       <modal modal-name="rest-modal">
         <template v-slot:modal-body>Choose your rest type</template>
         <template v-slot:modal-footer>
           <div>
-            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="longRest">Long
-              rest
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-dismiss="modal"
+              @click="longRest"
+            >
+              Long rest
             </button>
-            <button type="button" class="btn btn-primary" data-dismiss="modal" @click="shortRest">Short
-              rest
+            <button
+              type="button"
+              class="btn btn-primary"
+              data-dismiss="modal"
+              @click="shortRest"
+            >
+              Short rest
             </button>
           </div>
         </template>
@@ -31,12 +45,12 @@
 <script lang="ts">
 import Vue from "vue";
 import Modal from "@/components/Modal.vue";
-import {mapMutations} from "vuex";
+import { mapMutations } from "vuex";
 
 export default Vue.extend({
   name: "NavBar",
-  components: {Modal},
-  props: {appTitle: String},
+  components: { Modal },
+  props: { appTitle: String },
   methods: {
     changeMode(): void {
       this.$store.commit("changeMode");
@@ -51,13 +65,15 @@ export default Vue.extend({
       });
       this.changeMode();
     },
-    ...mapMutations(["longRest", "shortRest"])
+    ...mapMutations(["longRest", "shortRest"]),
   },
 });
 </script>
 
 <style scoped>
 #container {
+  position: fixed;
+  top: 0;
   width: 100%;
   font-family: "Heebo", serif;
   display: flex;
